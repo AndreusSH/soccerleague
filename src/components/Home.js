c7a5ba75a17961943fdf3e6ba13fc3c03ffb9f9b
AndreusSH <andreamammoliti@yahoo.it>
2023-08-26 11:26:13 +0200

    Create Home.js structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,22 +7,13 @@ function Home(){
         <div>
             <h1>Upcoming Events</h1>
         <p>
-        August 4
-
-NYSL Fundraiser
+        August 4 NYSL Fundraiser
         </p>
         <p>
-            
-August 16
-
-Season Kick-off: Meet the Teams
+            August 16 Season Kick-off: Meet the Teams
         </p>
         <p>
-            
-
-September 1
-
-First Game of the Season (Check Game Schedule for details)
+        September 1 First Game of the Season (Check Game Schedule for details)
         </p>
         </div>
     )
