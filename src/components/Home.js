c8ba0cd7659284dd63e23553feeef73648ff429a
AndreusSH <andreamammoliti@yahoo.it>
2023-08-26 11:22:35 +0200

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+function Home(){
+
+ 
+
+
+    return (
+        <div>
+            <h1>Upcoming Events</h1>
+        <p>
+        August 4
+
+NYSL Fundraiser
+        </p>
+        <p>
+            
+August 16
+
+Season Kick-off: Meet the Teams
+        </p>
+        <p>
+            
+
+September 1
+
+First Game of the Season (Check Game Schedule for details)
+        </p>
+        </div>
+    )
+
+
+}
+
+export default Home;
