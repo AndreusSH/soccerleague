4613181961b7cef70bbc69b34ab5d6a4bb5cd9a9
AndreusSH <andreamammoliti@yahoo.it>
2023-08-29 10:53:24 +0200

    display table

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Games.css';
 import eventsData from './events.json';
+import { useState } from 'react';
  
 
 
@@ -11,38 +12,41 @@ const capitalize = (word) => {
 return word[0].toUpperCase() + word.slice(1);
 }
 
+
 const renderHeader = () => {
     const firstEvent = eventsData[Object.keys(eventsData)[0]][0];
         return Object.keys(firstEvent)
         .filter(key => key !== "address")
-        .map((key, index) => <th scope="col" key={index}>{capitalize(key)}</th>);
+        .map((key, index) => <th  scope ="col" key={index}>{capitalize(key)}</th>);
 
         
 }
 
 const renderEvents = () => {
     let previousDate = null;
+     
+    return Object.keys(eventsData).map(dateKey => (
+      eventsData[dateKey].map((entry, index) => {
+        const currentDate = entry.date;
+        const showDate = currentDate !== previousDate;
+        previousDate = currentDate;
+  
+ 
+          return (
+            <tr key={`${dateKey}_${index}`}>
+              <td>{showDate ? entry.date : ""}</td>
+              <td>{entry.team}</td>
+              <td>{entry.Location}</td>
+              <td>{entry.time}</td>
+            </tr>
+          );
+        
+        
+    
+      })
+    ))
+  }
 
-    return Object.entries(eventsData).map(([date, events]) => (
-        <div key={date}>
-            {events.map((event, index) => {
-                const currentDate = event.date;
-                const showDate = currentDate !== previousDate;
-                previousDate = currentDate;
-
-                return (
-                    <tr key={index}>
-                        
-                        <td>{showDate ? currentDate : ''}</td>
-                        <td>{event.team}</td>
-                        <td>{event.Location}</td>
-                        <td>{event.time}</td>
-                    </tr>
-                );
-            })}
-        </div>
-    ));
-}
 
 function Games() {
     //console.log(eventsData);
@@ -50,14 +54,11 @@ function Games() {
     return (
         <div>
             <h1>NYSL Game Information</h1>
-            <table class="table-styling">
-            <thead>
-          <tr>{renderHeader()}</tr>
-        </thead>
-   <tbody>
-           
-                    {renderEvents()}
-                    </tbody>
+            <table class="table">
+                <thead>
+                  <tr>{renderHeader()}</tr>
+                </thead>
+            <tbody>{renderEvents()}</tbody>
                </table>
         </div>
     );
