210b5b202f3f48e0b0759cdfe15d2755a824ad06
AndreusSH <andreamammoliti@yahoo.it>
2023-09-26 10:59:14 +0200

    Toggle button using react-switch library

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import "../index.css";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import Button from "./Button";
+import ReactSwitch from "react-switch";
  
  
 function Navbar() {
@@ -10,7 +11,10 @@ function Navbar() {
   return (
    <header className="bg-white sticky top-0 z-[20] mx-auto flex flex-wrap w-full items-center text-white justify-between lg:border-b  border-gray-300">
     <Logo/>
+
     <Nav/>
+    <ReactSwitch/>
+
    </header>
  
   );
