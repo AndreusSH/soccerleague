c490911cb6e485ca75bbfdfa88ebbd63677570f1
AndreusSH <andreamammoliti@yahoo.it>
2023-09-27 15:50:04 +0200

    fixing the button

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,13 @@
-import { signInWithGooglePopup } from "../utils/firebase"
+import { signInWithGooglePopup } from "../utils/firebase";
+import '../index.css';
 
 const SignIn = () => {
 
 return (
         <div>
-            <button onClick={signInWithGooglePopup} className=" signIn font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            <button onClick={signInWithGooglePopup} className="signIn py-2 px-4 border
+      hover:border-transparent rounded
+     text-xs sm:text-sm   lg:text-base lg:ml-0">
   Log In
 </button>
         </div>
